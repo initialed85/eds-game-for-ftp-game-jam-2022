3ec00e668ef64ab8e12b11e028a289d67eabe1f4
Edward Beech <initialed85@gmail.com>
2023-01-07 21:38:20 +0800

    Tweak kube stuff, disable mobile device input hacks for now

--- src/client/setup.rs
+++ src/client/setup.rs
@@ -1,28 +1,30 @@
 use bevy::prelude::{Commands, ResMut};
 
-use crate::client::input::spawn_button;
-use crate::constants::{
-    UI_BUTTON_BOTTOM_LEFT_LEFT, UI_BUTTON_BOTTOM_LEFT_TOP, UI_BUTTON_BOTTOM_RIGHT_LEFT,
-    UI_BUTTON_BOTTOM_RIGHT_TOP,
-};
+// use crate::client::input::spawn_button;
+// use crate::constants::{
+//     UI_BUTTON_BOTTOM_LEFT_LEFT, UI_BUTTON_BOTTOM_LEFT_TOP, UI_BUTTON_BOTTOM_RIGHT_LEFT,
+//     UI_BUTTON_BOTTOM_RIGHT_TOP,
+// };
 use crate::identity::game::Game;
 
-pub fn handle_setup(mut game: ResMut<Game>, mut commands: Commands) {
+pub fn handle_setup(mut game: ResMut<Game>, mut _commands: Commands) {
     game.role = "client".to_string();
 
-    spawn_button(
-        &mut commands,
-        UI_BUTTON_BOTTOM_LEFT_LEFT,
-        UI_BUTTON_BOTTOM_LEFT_TOP,
-        true,
-        false,
-    );
+    // TODO
+    // spawn_button(
+    //     &mut commands,
+    //     UI_BUTTON_BOTTOM_LEFT_LEFT,
+    //     UI_BUTTON_BOTTOM_LEFT_TOP,
+    //     true,
+    //     false,
+    // );
 
-    spawn_button(
-        &mut commands,
-        UI_BUTTON_BOTTOM_RIGHT_LEFT,
-        UI_BUTTON_BOTTOM_RIGHT_TOP,
-        false,
-        true,
-    );
+    // TODO
+    // spawn_button(
+    //     &mut commands,
+    //     UI_BUTTON_BOTTOM_RIGHT_LEFT,
+    //     UI_BUTTON_BOTTOM_RIGHT_TOP,
+    //     false,
+    //     true,
+    // );
 }
